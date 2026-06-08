e4d832641139a71c6c4b22983834492b961e0e8a
Rancho-08 <likhitranchhod9082@gmail.com>
2026-06-08 15:01:43 +0530

    Create solution4.py
    Solved the given problem

---
+++ students/likhit-ranchod/solution4.py
@@ -0,0 +1,22 @@
+#problem1
+
+num=67
+unum=int(input("Enter your number:"))
+if unum<num:
+    print("Too Low")
+elif unum>num:
+    print("Too High")
+else:
+    print("correct guess good job ")
+
+
+#Problem2
+
+num=19
+guess=int(input("Guess the number:"))
+while guess != num:
+  print("Wrong Guess. Try again")
+  guess=int(input("Guess the number:"))
+
+if guess == num:
+  print("You guessed correctly good job")
